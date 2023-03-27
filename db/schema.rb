3438dd6a8cfe2337b60a00573488183845b379bc
shivanisolanki <shivanisolanki9691@gmail.com>
2023-03-27 17:46:06 +0530

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_094053) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_071349) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,8 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_094053) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
- 
-
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -49,7 +50,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_094053) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "todos", force: :cascade do |t|
+    t.string "name"
+    t.string "day"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.string "password_confirmation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
- 
 end
